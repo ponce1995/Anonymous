a6f4026280edd1b1845e51272fcf869f30f1f842
Administrador <Administrador@172.16.12.110>
2017-09-07 14:53:57 -0600

    ejemplo de practica

---
+++ src/PAQUETE/pruebaRepo.java
@@ -0,0 +1,8 @@
+package PAQUETE;
+
+
+public class pruebaRepo {
+    public static void main(String args) {
+        System.out.println("Hola Mundo con  Java");
+    }
+}
---
+++ src/ejemplosrepositores/EjemplosREPOSITORES.java
@@ -0,0 +1,11 @@
+package ejemplosrepositores;
+
+
+public class EjemplosREPOSITORES {
+
+
+    public static void main(String[] args) {
+       //PRUEBAGITHUB
+    }
+
+}
